983aca075ad585274259f9fdf6f876bb7be34872
Your name here <none@example.com>
2019-07-01 14:32:28 +0200

    Sagi Antebi

---
+++ app/src/main/java/de/berlindroid/zeaapp/LuckySplashActivity.kt
@@ -0,0 +1,61 @@
+package de.berlindroid.zeaapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.widget.TextView
+
+class LuckySplashActivity : AppCompatActivity(), RandomizerInterface {
+
+    override fun onRandomNumber(number: Int) {
+        textView?.setText("${number}")
+        if (number == 7 || number == 77 || number == 5) {
+            startActivity(Intent(this, IamTheRealSplashActivity::class.java))
+            randomizer.stop()
+        }
+    }
+
+    var textView: TextView? = null
+
+    val randomizer:Randomizer = Randomizer(this)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lucky_splash)
+        textView = findViewById(R.id.textView)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        randomizer.start()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        randomizer.stop()
+    }
+
+
+    class Randomizer(val listener: RandomizerInterface) {
+
+        val handler = Handler(Looper.getMainLooper())
+
+        val runnable  = Runnable {
+            listener.onRandomNumber((Math.random() * 100).toInt())
+            start()
+        }
+
+        fun start() {
+           handler.postDelayed(runnable, 500)
+        }
+
+        fun stop() {
+            handler.removeCallbacks(runnable)
+        }
+
+    }
+
+
+}
---
+++ app/src/main/java/de/berlindroid/zeaapp/RandomizerInterface.kt
@@ -0,0 +1,5 @@
+package de.berlindroid.zeaapp
+
+interface RandomizerInterface {
+    fun onRandomNumber(number:Int)
+}
