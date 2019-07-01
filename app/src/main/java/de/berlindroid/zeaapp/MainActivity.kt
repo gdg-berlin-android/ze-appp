14efeee3f834dd00742d5c6dce0f84692528314b
Your name here <none@example.com>
2019-07-01 12:55:21 +0200

    Satyan

--- app/src/main/java/de/berlindroid/zeaapp/MainActivity.kt
+++ app/src/main/java/de/berlindroid/zeaapp/MainActivity.kt
@@ -2,7 +2,10 @@ package de.berlindroid.zeaapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.android.material.snackbar.Snackbar
 import de.berlindroid.zeaapp.api.ZeApppApi
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,6 +13,22 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        setupListener()
 //        App.retrofit.create(ZeApppApi.javaClass).
+    }
+
+    private fun setupListener() {
+        main_play_button.setOnClickListener {
+            Snackbar.make(main_container, getString(R.string.main_lose_text), Snackbar.LENGTH_LONG)
+                .setAction(getString(R.string.main_lose_action)) {
+                    Toast.makeText(this, getString(R.string.main_baby_loser), Toast.LENGTH_LONG).show()
+                    finish()
+                }
+                .show()
+        }
+        main_play_button.setOnLongClickListener {
+            Snackbar.make(main_container, getString(R.string.main_zin), Snackbar.LENGTH_LONG).show()
+            true
+        }
     }
 }
