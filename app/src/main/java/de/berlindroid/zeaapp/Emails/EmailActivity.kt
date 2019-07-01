f8d29d47aa1aaeab75d19eecf695161f9d0ee3f5
Your name here <none@example.com>
2019-07-01 17:02:35 +0200

    fixes

--- app/src/main/java/de/berlindroid/zeaapp/Emails/EmailActivity.kt
+++ app/src/main/java/de/berlindroid/zeaapp/Emails/EmailActivity.kt
@@ -1,15 +1,14 @@
 package de.berlindroid.zeaapp.Emails
 
 import android.os.Bundle
-import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
 import de.berlindroid.zeaapp.R
 
-class EmailActivity: AppCompatActivity() {
+class EmailActivity : AppCompatActivity() {
 
 
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
         setContentView(R.layout.email_activity)
     }
 }
