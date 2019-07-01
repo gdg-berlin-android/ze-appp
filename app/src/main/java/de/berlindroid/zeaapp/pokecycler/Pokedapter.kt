2bf47868795ddcce41b3a24c42a56c81539afb65
Your name here <none@example.com>
2019-07-01 17:36:36 +0200

    Ataul Munim

--- app/src/main/java/de/berlindroid/zeaapp/pokecycler/Pokedapter.kt
+++ app/src/main/java/de/berlindroid/zeaapp/pokecycler/Pokedapter.kt
@@ -1,12 +1,20 @@
 package de.berlindroid.zeaapp.pokecycler
 
+import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.widget.TooltipCompat
+import androidx.core.view.AccessibilityDelegateCompat
+import androidx.core.view.ViewCompat
+import androidx.core.view.accessibility.AccessibilityNodeInfoCompat
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import de.berlindroid.zeaapp.R
 import kotlinx.android.synthetic.main.item_pokemon.view.*
+import java.lang.RuntimeException
 
 class Pokedapter : RecyclerView.Adapter<Pokeholder>() {
 
@@ -19,11 +27,15 @@ class Pokedapter : RecyclerView.Adapter<Pokeholder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Pokeholder {
 // TODO: do the layout please
         val view = LayoutInflater.from(parent.context).inflate(R.layout.item_pokemon, parent, false)
+
         return Pokeholder(view)
     }
 
     override fun onBindViewHolder(holder: Pokeholder, position: Int) {
         holder.bind(pokemons[position])
+        var view = holder.itemView
+        ViewCompat.setAccessibilityDelegate(view, PokeAccessibilityDelegate())
+        TooltipCompat.setTooltipText(view, view.toastText())
     }
 
 }
@@ -35,5 +47,48 @@ data class Pokemon(
 class Pokeholder(val view: View) : RecyclerView.ViewHolder(view) {
     fun bind(pokemon: Pokemon) {
         itemView.pokeName.text = pokemon.name
+    }
+}
+
+private fun View?.toastText(): CharSequence {
+    return (this?.findViewById<TextView>(R.id.pokeName))?.text ?: "Not a TextView"
+}
+
+private const val ACTION_TOAST_NAME = 348732
+class PokeAccessibilityDelegate : AccessibilityDelegateCompat() {
+
+    override fun onInitializeAccessibilityNodeInfo(
+        host: View?,
+        info: AccessibilityNodeInfoCompat?
+    ) {
+        super.onInitializeAccessibilityNodeInfo(host, info)
+        AccessibilityNodeInfoCompat.AccessibilityActionCompat(ACTION_TOAST_NAME, "Toast ${host.toastText()}")
+        info?.addAction(ACTION_TOAST_NAME)
+    }
+
+
+
+    override fun performAccessibilityAction(host: View?, action: Int, args: Bundle?): Boolean {
+        return when (action) {
+            ACTION_TOAST_NAME -> {
+                host?.let{
+                    toast(it, host.toastText().toString())
+                }
+                true
+            }
+            else -> super.performAccessibilityAction(host, action, args)
+        }
+    }
+
+    private fun toast(view:View, message:String) {
+        toast?.cancel()
+        Toast.makeText(view.context, message, Toast.LENGTH_SHORT).apply {
+            toast = this
+            show()
+        }
+    }
+
+    companion object {
+        private var toast: Toast? = null
     }
 }
