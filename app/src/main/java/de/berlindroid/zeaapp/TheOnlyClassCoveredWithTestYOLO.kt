fa15a5dafbadc97ff570f9d2a70963e1394a0ede
Your name here <none@example.com>
2019-07-02 15:38:17 +0200

    Rishad Mustafaev

---
+++ app/src/main/java/de/berlindroid/zeaapp/TheOnlyClassCoveredWithTestYOLO.kt
@@ -0,0 +1,24 @@
+package de.berlindroid.zeaapp
+
+import android.os.CountDownTimer
+
+class TheOnlyClassCoveredWithTestYOLO(
+    val onTickAction: ((String) -> Unit)? = null,
+    val onFinishCountdownAction: () -> Unit,
+    durationInMillis: Long = 5000,
+    tickIntervalInMillis: Long = 500
+) : CountDownTimer(durationInMillis, tickIntervalInMillis) {
+
+    override fun onFinish() {
+        onFinishCountdownAction()
+    }
+
+    override fun onTick(timeLeft: Long) {
+        val secondsLeft = if(timeLeft / 1000 < 1){
+            0
+        } else{
+            timeLeft/1000
+        }
+        onTickAction?.invoke("ACHTUNG!!! Time left: $secondsLeft")
+    }
+}
---
+++ app/src/test/java/de/berlindroid/zeaapp/TheOnlyTestClassHereDoNotTouch.kt
@@ -0,0 +1,40 @@
+package de.berlindroid.zeaapp
+
+import android.os.CountDownTimer
+import org.junit.Before
+import org.junit.Test
+import org.mockito.ArgumentMatchers.argThat
+import org.mockito.Mock
+import org.mockito.Mockito.atLeastOnce
+import org.mockito.Mockito.verify
+import org.mockito.MockitoAnnotations.initMocks
+
+class TheOnlyTestClassHereDoNotTouch {
+
+    lateinit var classUnderTest : CountDownTimer
+
+    @Mock
+    lateinit var onTickActionMock: (String) -> Unit
+
+    @Mock
+    lateinit var onFinishTimerMock: () -> Unit
+
+    @Before
+    fun setup(){
+        initMocks(this)
+        classUnderTest = TheOnlyClassCoveredWithTestYOLO(
+            onTickAction = onTickActionMock,
+            onFinishCountdownAction = onFinishTimerMock)
+    }
+
+    @Test
+    fun onTickActionActuallyInvoked(){
+        classUnderTest.onTick(4000)
+    }
+
+    @Test
+    fun onFinishActionActuallyInvoked(){
+        classUnderTest.onFinish()
+        verify(onFinishTimerMock, atLeastOnce()).invoke()
+    }
+}
