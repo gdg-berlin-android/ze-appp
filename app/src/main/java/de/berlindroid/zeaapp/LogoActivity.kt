4317a7d4d95800d3543bdd51523a26e5faefa4e1
Your name here <none@example.com>
2019-07-02 16:01:48 +0200

    Nick Rout

---
+++ app/src/main/java/de/berlindroid/zeaapp/LogoActivity.kt
@@ -0,0 +1,12 @@
+package de.berlindroid.zeaapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class LogoActivity : AppCompatActivity(R.layout.activity_logo) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
